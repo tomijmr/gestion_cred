b005de2f1c8b3cd708d283fa63dd93d5064cf4e4
Tomás Canavidez <tms1canavidez@gmail.com>
2025-06-16 03:35:25 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,24 +1,10 @@
 <?php
 session_start();
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "gestion_creditos";
-
-$conn = new mysqli($host, $user, $password, $dbname);
-$conn->set_charset("utf8");
-
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
-
-// session_start();
-
 // $host = "localhost";
-// $user = "c2611613_gescred";
-// $password = "SI42dakize";
-// $dbname = "c2611613_gescred";
+// $user = "root";
+// $password = "";
+// $dbname = "gestion_creditos";
 
 // $conn = new mysqli($host, $user, $password, $dbname);
 // $conn->set_charset("utf8");
@@ -26,4 +12,18 @@ if ($conn->connect_error) {
 // if ($conn->connect_error) {
 //     die("Error de conexión: " . $conn->connect_error);
 // }
+
+session_start();
+
+$host = "localhost";
+$user = "c2611613_gescred";
+$password = "SI42dakize";
+$dbname = "c2611613_gescred";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+$conn->set_charset("utf8");
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
 ?>
